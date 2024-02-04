1e300ae1fab2d86ff92c559d795da1ff5cda4d26
Cassidy <cassidy0kovell@gmail.com>
2024-02-03 16:30:11 -0800

    added list items for the locally stored previous cities the user searched for

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 var searchBtn = document.getElementById("getWeather");
 var cityInput = document.getElementById("searchInput");
-var today = document.getElementById("today-info");
-var fiveDay = document.getElementById("five-day");
 var todayTemp = document.getElementById('today-temp');
-var todayWind =document.getElementById('today-wind');
-var todayHum =document.getElementById('today-hum');
+var todayWind = document.getElementById('today-wind');
+var todayHum = document.getElementById('today-hum');
+var city = document.getElementById('city');
+var forecastCity = document.getElementById('five-day-city');
+var previousCities = [];
 
 var APIKey = "bfc41f2a23f49b411a509bc0b6a9d463";
-
-var city = "";
-
-searchBtn.addEventListener("click", function () {
-  fetchWeatherData();
-});
 
 function fetchWeatherData() {
   var cityName = cityInput.value;
@@ -26,6 +21,7 @@ function fetchWeatherData() {
       var lat = data[0].lat;
       var lon = data[0].lon;
       var weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
+
       fetch(weatherApiUrl)
         .then(function (response) {
           return response.json();
@@ -37,30 +33,60 @@ function fetchWeatherData() {
     });
 }
 
-function weatherInfo(weatherForcastData) {
-    weatherForcastData.list[0];
-    var todaysTempurature = weatherForcastData.list[0].main.temp;
-    var todaysWind = weatherForcastData.list[0].wind.speed;
-    var todaysHumidity = weatherForcastData.list[0].main.humidity;
-    todayTemp.innerHTML = `Tempurature: ${todaysTempurature}°C`
-    todayWind.innerHTML = `Wind: ${todaysWind} M/S`
-    todayHum.innerHTML = `Humidity: ${todaysHumidity} %`
+function weatherInfo(weatherForecastData) {
+  var todaysTemperature = weatherForecastData.list[0].main.temp;
+  var todaysWind = weatherForecastData.list[0].wind.speed;
+  var todaysHumidity = weatherForecastData.list[0].main.humidity;
+
+  city.innerHTML = cityInput.value;
+  todayTemp.innerHTML = `Temperature: ${todaysTemperature}°C`;
+  todayWind.innerHTML = `Wind: ${todaysWind} M/S`;
+  todayHum.innerHTML = `Humidity: ${todaysHumidity} %`;
 }
 
-function fiveDayForcast(weatherForcastData) {
-    fiveDay.innerHTML=''
-    for (i = 0; i < weatherForcastData.list.length; i+=8 ) {
-        console.log(weatherForcastData.list[i])
-        var singleDayForcast = ` <div class="col mb-3">
+function fiveDayForcast(weatherForecastData) {
+  var fiveDay = document.getElementById('five-day');
+  fiveDay.innerHTML = '';
+
+  for (var i = 0; i < weatherForecastData.list.length; i += 8) {
+    var singleDayForecast = `
+      <div class="col mb-3">
         <div class="background card border-0">
-            <div class="p-3 text-black">
-                <p class="fw-semibold">( ______ )</p>
-                <p class="my-3 mt-3">Temp:${weatherForcastData.list[i].main.temp}°C</p>
-                <p class="my-3">Wind: ${weatherForcastData.list[i].wind.speed} M/S</p>
-                <p class="my-3">Humidity: ${weatherForcastData.list[i].main.humidity}%</p>
-            </div>
+          <div class="p-3 text-black">
+            <p class="fw-semibold">( ______ )</p>
+            <p class="my-3 mt-3">Temp: ${weatherForecastData.list[i].main.temp}°C</p>
+            <p class="my-3">Wind: ${weatherForecastData.list[i].wind.speed} M/S</p>
+            <p class="my-3">Humidity: ${weatherForecastData.list[i].main.humidity}%</p>
+          </div>
         </div>
-    </div>`
-    fiveDay.innerHTML+=singleDayForcast
-    }
+      </div>`;
+
+    fiveDay.innerHTML += singleDayForecast;
+  }
 }
+
+function displayPreviousCities() {
+  var previousCitiesList = document.getElementById('previousCities');
+  previousCitiesList.innerHTML = '';
+  previousCitiesList.style.margin = '0';
+  previousCitiesList.style.padding = '0';
+
+  var storedCities = localStorage.getItem('previousCities');
+  if (storedCities) {
+    var parseCities = JSON.parse(storedCities);
+    parseCities.forEach(function (city) {
+      var list = document.createElement('li');
+      list.textContent = city;
+      list.classList.add('list-item', 'col-12', 'col-md-4', 'col-lg-4');
+      previousCitiesList.appendChild(list);
+    });
+  }
+}
+
+searchBtn.addEventListener("click", function () {
+  fetchWeatherData();
+  var city = cityInput.value;
+  previousCities.push(city);
+  localStorage.setItem('previousCities', JSON.stringify(previousCities));
+  displayPreviousCities();
+});
